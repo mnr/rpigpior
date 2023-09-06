1e52ad7aedf85a2045c7666e5aeb42b9b79c60b5
Mark Niemann-Ross <mark.niemannross@gmail.com>
2023-09-06 00:52:58 -0700

    document multiple lines

--- R/gpioget.R
+++ R/gpioget.R
@@ -3,7 +3,7 @@
 # https://kernel.googlesource.com/pub/scm/libs/libgpiod/libgpiod/+/v0.2.x/README.md
 #' gpioget
 #'
-#' @param gpio_line Retrieve state from this pin. Uses BCM numbering.
+#' @param gpio_line Retrieve state from this pin. Uses BCM numbering. This can be a single number or a vector or line numbers
 #' @param gpio_chip Retrieve the pin info from this chip number. Defaults to chip 0. Chips are listed by gpiodetect()
 #' @param gpio_active Set the line active state, or what value to return when signal is high. gpio_active = FALSE inverts the active state of the line
 #' @param gpio_bias Set the line bias to one of the following: as-is (the default), disable, pull-down, pull-up
@@ -13,9 +13,9 @@
 #'
 #' @examples
 #' read the value of a single gpio line
-#' gpioget gpiochip1 23
-#' Read two values at the same time. Set the active state of the lines to low.
-#' gpioget --active-low gpiochip1 23 24
+#' gpioget(gpio_chip = 0, gpio_line = 21)
+#' Read two values at the same time.
+#' gpioget(gpio_chip = 0, gpio_line = c(21,19))
 gpioget <- function(gpio_chip = 0,
                     gpio_active = TRUE,
                     gpio_line,
