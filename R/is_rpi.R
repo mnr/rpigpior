ba48d5446265c146f1d0fec566ac383c10872438
Mark Niemann-Ross <mark.niemannross@gmail.com>
2023-09-06 10:04:07 -0700

    initial commit

---
+++ R/is_rpi.R
@@ -0,0 +1,12 @@
+#' is.rpi()
+#'
+#' @return TRUE if running on a Raspberry Pi
+#' @export
+#'
+#' @examples
+is.rpi <- function(onlyThis = "NAME") {
+  system("cat /etc/os-release", intern = TRUE) |>
+    grep(paste0("^",onlyThis,"=")) |>
+    strsplit(split = "=") |>
+    unlist()
+}
