eb7ca93cf5a10b5a654c572b08725a6cd27d6ce3
Mark Niemann-Ross <mark.niemannross@gmail.com>
2023-09-07 14:41:33 -0700

    add ability to specify chip by name: gpiochip0

--- R/gpioget.R
+++ R/gpioget.R
@@ -4,7 +4,7 @@
 #' Retrieves the state of a single pin on the GPIO
 #'
 #' @param gpio_line Retrieve state from this pin. Uses BCM numbering. This can be a single number or a vector or line numbers
-#' @param gpio_chip Retrieve the pin info from this chip number. Defaults to chip 0. Chips are listed by gpiodetect()
+#' @param gpio_chip Retrieve the pin info from this chip number or chip specification. Defaults to chip 0. This can be a number or a name such as gpiochip0. Chips are listed by gpiodetect()
 #' @param gpio_active Set the line active state, or what value to return when signal is high. gpio_active = FALSE inverts the active state of the line
 #' @param gpio_bias Set the line bias to one of the following: as-is (the default), disable, pull-down, pull-up
 #'
@@ -15,8 +15,11 @@
 #' # read the value of a single gpio line
 #' gpioget(gpio_chip = 0, gpio_line = 21)
 #'
-#' # Read two values at the same time.
+#' # Read two values at the same time
 #' gpioget(gpio_chip = 0, gpio_line = c(21,19))
+#'
+#' # Specify gpiochip by name
+#' gpioget(gpio_chip = "gpiochip0", gpio_line = 21)
 gpioget <- function(gpio_chip = 0,
                     gpio_active = TRUE,
                     gpio_line,
@@ -29,6 +32,3 @@ gpioget <- function(gpio_chip = 0,
   # print(gpio_sysCall)
   return(system(gpio_sysCall, intern = TRUE))
 }
-
-# todo
-# gpio_chip should understand the output of gpiofind. i.e. "gpiochip0 15"
