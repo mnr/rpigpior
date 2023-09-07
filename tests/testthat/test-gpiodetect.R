b1c060a488b9422a5a507799d3ea37c1d1a0d248
Mark Niemann-Ross <mark.niemannross@gmail.com>
2023-09-07 14:26:44 -0700

    added is.rpi() condition

--- tests/testthat/test-gpiodetect.R
+++ tests/testthat/test-gpiodetect.R
@@ -1,4 +1,7 @@
 test_that("gpiodetect returns the gpiochips", {
   # this will obviously fail if libgpiod isn't installed
-  expect_no_error(gpiodetect())
+  expect_no_error(
+    if (is.rpi()) {
+      gpiodetect()
+      })
 })
