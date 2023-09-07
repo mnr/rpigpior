8d78f71a4545f5c7cba8493da6c3256c91c01b4d
Mark Niemann-Ross <mark.niemannross@gmail.com>
2023-09-07 14:41:47 -0700

    update roxygen

--- R/gpioinfo.R
+++ R/gpioinfo.R
@@ -1,14 +1,17 @@
 # gpioinfo - list all lines of specified gpiochips, their names, consumers, direction, active state and additional flags
 # https://git.kernel.org/pub/scm/libs/libgpiod/libgpiod.git/tree/tools/gpioinfo.c
 
-#' List all lines of specified gpiochips, their names, consumers, direction, active state and additional flags
+#' List all lines of a gpio chip
+#'
+#' List all lines of specified gpio chips, their names, consumers, direction, active state and additional flags
 #'
 #' @param gpio_chip The GPIO chip to search. Probably 0, 1, or something like gpiochip0
 #'
-#' @return
+#' @return a character vector. The first element identifies the chip and number of lines. The following elements consist of columns for line number, line name, used/unused status, input/output, active-high/active-low
 #' @export
 #'
-#' @examples
+#' @examplesIf is.rpi()
+#' gpioinfo(0)
 gpioinfo <- function(gpio_chip) {
   gpio_sysCall <- paste("gpioinfo",gpio_chip)
   system(gpio_sysCall, intern = TRUE)
