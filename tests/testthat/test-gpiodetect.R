c83175dc7e7a70ee72bf01e334028a11f13311ac
Mark Niemann-Ross <mark.niemannross@gmail.com>
2023-09-08 15:21:29 -0700

    abort if this isn't raspbian

--- tests/testthat/test-gpiodetect.R
+++ tests/testthat/test-gpiodetect.R
@@ -1,7 +1,5 @@
 test_that("gpiodetect returns the gpiochips", {
   # this will obviously fail if libgpiod isn't installed
-  expect_no_error(
-    if (is.rpi()) {
-      gpiodetect()
-      })
+  skip_if_not(is.rpi(), "gpiodetect - is.rpi() failed")
+  expect_no_error( gpiodetect())
 })
