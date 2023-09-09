26a44a1b7991f5e9f8491c08ce96865efe9982f6
Mark Niemann-Ross <mark.niemannross@gmail.com>
2023-09-09 10:54:33 -0700

    simplify the return value. Don't overthink this...

--- R/gpioinfo.R
+++ R/gpioinfo.R
@@ -3,12 +3,20 @@
 
 #' List all lines of a gpio chip
 #'
-#' List all lines of specified gpio chips, their names, consumers, direction, active state and additional flags
+#' List all lines of specified gpio chips, their names, consumers, direction, active state and additional flags. Be aware not all of these lines are available to the Raspberry Pi pins.
 #'
 #' @param gpio_chip The GPIO chip to search. Probably 0, 1, or something like gpiochip0
 #'
-#' @return a data.frame. The first row identifies the chip and number of lines.
-#'     The following rows identify line number, line name, used/unused status, input/output, active-high/active-low.
+#' @return a vector. The first element identifies the chip and number of lines.
+#'     The following elements contain information about each gpio line
+#'
+#' \itemize{
+#'   \item Line Number - The BCM number. This may be different than pinout number. For example, Raspberry Pi Pin 1 is 3.3 volts, not ID_SCL
+#'   \item Line Name - The descriptive name of this line. For example, BCM Line 0 is identified as ID_SDA and shows up as Raspberry Pi pin 3
+#'   \item Used/unused status - Is something talking to this line?
+#'   \item Input/Output mode - Some Raspberry Pi pins can be set to input or output
+#'   \item Active-High, Active-Low - When this line is closed, does it indicate ON or OFF?
+#' }
 #'
 #' @export
 #'
@@ -18,22 +26,5 @@ gpioinfo <- function(gpio_chip) {
   # read gpio info
   gpio_sysCall <- paste("gpioinfo",gpio_chip)
   allLines <- system(gpio_sysCall, intern = TRUE)
-
-  # set up receiver data.frame
-  df_forReturn_names <- c("line","name","usedUnused","input_output","active")
-  df_forReturn <- data.frame(matrix(ncol = length(df_forReturn_names), nrow = 0))
-  names(df_forReturn) <- df_forReturn_names
-
-  # read each line into df_forReturn
-  for(aLine in allLines) {
-    gInfoLine <- substr(aLine, start = 8, stop = 10)
-    gInfoName <- substr(aLine, start = 11, stop = 28)
-    gInfoUsedUnused <- substr(aLine, start = 30, stop = 36)
-    gInfoInOut <- substr(aLine, start = 37, stop = 43)
-    gInfoActive <- substr(aLine, start = 44, stop = 54)
-
-    tmpDF <- data.frame(gInfoLine, gInfoName, gInfoUsedUnused, gInfoInOut, gInfoActive)
-    df_forReturn <- rbind(df_forReturn, tmpDF)
-  }
-  return(df_forReturn)
+  return(allLines)
 }
