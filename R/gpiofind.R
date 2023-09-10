30b936af361cce1f0fe262078e7209a55d9fe303
Mark Niemann-Ross <mark.niemannross@gmail.com>
2023-09-09 18:55:41 -0700

    gpiofind only finds. Don't use it for gpioget - instead, use gpioget("GPIO21") which invokes gpiofind and processes the return

--- R/gpiofind.R
+++ R/gpiofind.R
@@ -1,18 +1,20 @@
 # gpiofind - find the gpiochip line by name.
-# the output of this command can be used as input for gpioget & gpioset
+#
 
 #' Find the chip and line for a named function
 #'
+#' Identifies the gpiochip and gpioline. For example, `gpiofind("SCL0")` will return "gpiochip0 45". Note that gpioset and gpioget both accept line names in place of line numbers and will identify the proper gpiochip using gpiofind behind the scenes.
+#'
 #' @param gpio_name
 #'
-#' @return The chip name and line number. This can be used as input for 'gpioget()'
+#' @return The chip name and line number.
 #' @export
 #'
 #' @examplesIf is.rpi()
 #'
-#' gpiofind(SCL0) # returns "gpiochip0 45"
+#' gpiofind("SCL0") # returns "gpiochip0 45"
 #'
-#' gpioget(gpiofind(SCL0)) # returns status of "gpiochip0 45"
+#' gpioget(gpiofind("SCL0")) # returns status of "gpiochip0 45"
 gpiofind <- function(gpio_name) {
   gpio_sysCall <- paste("gpiofind", gpio_name)
   system(gpio_sysCall, intern = TRUE)
