fcf859182de60fe37bd013aebc2ef01cc8613ef9
Mark Niemann-Ross <mark.niemannross@gmail.com>
2023-09-10 17:09:51 -0700

    adding documentation for rpi_pin_desc

--- R/data.R
+++ R/data.R
@@ -3,7 +3,7 @@
 #' A board-level description of the 40 GPIO pins found on a Raspberry Pi.
 #'     This is different than wiring pi or BCM numbering schemes. Instead, this reflects the function of pins by their physical location on the board.
 #'
-#' @format ## `r_pin_desc`
+#' @format ## `rpi_pin_desc`
 #' A data frame with 40 rows and two columns:
 #' \describe{
 #' \item {description}{secondary}
@@ -11,3 +11,4 @@
 #' }
 #'
 #' @source https://www.raspberrypi.com/documentation/computers/os.html#gpio-and-the-40-pin-header
+"rpi_pin_desc"
