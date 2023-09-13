87af1621dc25138de47aa11bbda2a9318ebee6fa
Mark Niemann-Ross <mark.niemannross@gmail.com>
2023-09-12 18:32:08 -0700

    first run at executable code

--- R/rpi_monitor.R
+++ R/rpi_monitor.R
@@ -1,8 +1,14 @@
+# system("gpiomon --num-events=10 0 21", intern=TRUE) returns...
+# event:   RISING EDGE offset: 21 timestamp:  [1051165.516409561]
+#
+# event type RISING EDGE | FALLING EDGE
+# timestamp = seconds.nanoseconds
+#
 # rpi_monitor <- function(pin_number) {
 #
 # }
 #
-# gpio_sysCall <- paste("gpioget",
+# gpio_sysCall <- paste("gpiomon",
 #                       if (!gpio_active) {
 #                         "--active-low"
 #                       },
@@ -23,3 +29,28 @@
 #                     format) {
 #
 # }
+rpi_monitor <- function(pin_number, numEvents = 0, edge = "", timeout = 0) {
+
+  bcm_line <- rpigpior::rpi_pinToBCM(pin_number)
+
+  gpio_sysCall <- paste("gpiomon",
+                        if (numEvents != 0 ) {
+                          paste0("--num-events=",numEvents)
+                        } else {
+                          paste0("--num-events=",10)
+                        },
+                        "--silent",
+                        if(edge == "rising") {
+                          "--rising-edge"
+                        } else if (edge == "falling") {
+                          "--falling-edge"
+                        },
+                        paste(bcm_line, collapse = " "))
+
+  monitored_events <- system(gpio_sysCall, intern = TRUE)
+
+  myPattern <- "event: +([A-Z ]+) offset: (\\d+) timestamp: \\[ (\\d+)\\.(\\d+)\\]"
+  rxmatches <- regexec(pattern=myPattern, text=monitored_events, perl=TRUE)
+  rxvalues <- regmatches(x=monitored_events, m = rxmatches)
+  return(rxvalues)
+  }
