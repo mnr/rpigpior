8ffe37e3a77d4d78f8aed52b20a5670bd730b231
Mark Niemann-Ross <mark.niemannross@gmail.com>
2023-09-17 13:20:24 -0700

    Error check for enabled PWM channels

--- R/rpi_pwm.R
+++ R/rpi_pwm.R
@@ -5,34 +5,30 @@
 #'     Only two PWM lines are available on the Raspberry Pi - pwm0 and pwm1. Although it is possible to create software PWM, this isn't advised due to latency of linux and R.
 #'     It might be helpful to read the article on `rpi_pwm()` located at the [rpigpior website](https://mnr.github.io/rpigpior/articles/rpi_pwm.html).
 #'
-#' @param pin_number. Select one or two of four hardware PWM lines (12, 32, 33, or 35). Generates an error message if a pwm channel is selected twice.
-#'     Since PWM0 appears on both pins 12 and 32, and PWM1 appears on pins 33 and 35, a valid selection of two pins is one from PWM0 and one from PWM1.
-#'     In other words, only these combinations are valid:
-#'     \itemize{
-#'        \item pin_number = c(12,33)
-#'        \item pin_number = c(32,33)
-#'        \item pin_number = c(12,35)
-#'        \item pin_number = c(32,35)
-#'     }
-#'
-#' @param mode. An activity related to PWM
-#'
-#'     \itemize{
-#'      \item help. Prints an informative message
-#'      \item enable. Prints instructions for enabling the selected PWM pin. This requires root access which R doesn't have
-#'    }
+#' @param pin_number. One or two pins for hardware PWM. Must be 12, 32, 33, or 35.
+#'    If two pins are selected, they must be one of these combinations: (12,33), (32,33), (12,35), or (32,35)
+#' @param pwm_period. The length of a cycle. aka Frequency
+#' @param pwm_dutycycle. The amount of time a cycle is on.
 #'
 #' @return void
+#'     `rpi_pwm()` provides extensive error checking. Possible errors include:
+#'     \itemize{
+#'      \item Invalid PWM pin: Hardware PWM is only supplied to pins 12, 32, 33, or 35.
+#'      \item Invalid PWM pin combination: The combination of pins both select PWM0 or PWM1
+#'      \item PWM not enabled: This PWM channel is not enabled
+#'      }
 #' @export
 #'
 #' @examplesIf is.rpi()
+#' rpi_pwm(12) # provides 50% PWM to pin 12 (PWM0)
 #'
+#' rpi_pwm(c(12,33), pwm_period = 50000, pwm_dutycycle = 10000) # provides 20% PWM to pin 12 (PWM0) and pin 33 (PWM1)
 #'
-rpi_pwm <- function(pin_number = 12, mode = "help") {
+rpi_pwm <- function(pin_number = 12, pwm_period = 50000, pwm_dutycycle = 25000 ) {
   # check that pin_number == 12, 32, 33, or 35
   for (aPin in pin_number) {
     if(!rpigpior::rpi_pin_desc[aPin,"valid_PWM_pair_1"]) {
-      stop(paste(pin_number, "is not a valid PWM channel. Use 12, 32, 33, or 35"))
+      stop(paste("Invalid PWM pin:", pin_number, "is not a valid PWM channel. Use 12, 32, 33, or 35"))
     }
   }
 
@@ -43,49 +39,60 @@ rpi_pwm <- function(pin_number = 12, mode = "help") {
          )
       ) {
            # the pin set isn't a valid combination
-           stop(paste(pin_number, "is not a valid PWM combination."))
+           stop(paste("Invalid PWM pin combination:", pin_number, "is not a valid PWM combination."))
         }
 
     }
 
   if (length(pin_number > 2)) {
-    stop("You have specified more than two pins for PWM.")
+    stop("Invalid PWM pin combination: You have specified more than two pins for PWM.")
   }
+
+
+# Is PWM enabled? ---------------------------------------------------------
+  for (aPin in pin_number) {
+    if (!(dir.exists(paste0("/sys/class/pwm/pwmchip0/pwm",
+                             rpigpior::rpi_pin_desc[aPin,"PWM_channel"]
+                             )
+                      )
+           )
+    ) {
+      # oops. This PWM channel isn't configured. Print a helpful message then stop
+      print("PWM not enabled:")
+
+      # Print a message that describes how to enable a pin
+      dtoverlayString <- if (length(pin_number) > 1) {
+                              "dtoverlay=pwm-2chan"
+                            } else {
+                              "dtoverlay=pwm"
+                            }
+
+      dtoverlayString <- paste0(dtoverlayString, ",")
+
+      for(aPin in pin_number) {
+          dtoverlayString <- paste0(dtoverlayString,
+                                     "pin=", rpigpior::rpi_pinToBCM(aPin), ",",
+                                     "func=", pwm_func(aPin), ","
+                                    )
+        }
+      dtoverlayString <- substring(dtoverlayString, first = 0, last=nchar(dtoverlayString)-1)# remove trailing comma
+
+      # dtoverlayString contains the string to place in /boot/config.txt
+      print(paste("Add this string to /boot/config:", dtoverlayString))
+
+      stop(paste("PWM not enabled: Channel", rpigpior::rpi_pin_desc[aPin,"PWM_channel"], "has not been enabled"))}
+    }
+
 } # end of rpi_pwm
 
-#
-#   # mode == "enable" --------------------------------------------------------
-#   # Print a message that describes how to enable a pin
-#   # first, check /boot/config.txt for "dtoverlay=pwm"
-#   # if "dtoverlay=pwm" isn't present, instruct user on how to enable
-#   dtoverlayString <- if (length(pin_number) > 1) {
-#     "dtoverlay=pwm-2chan"
-#   } else {
-#     "dtoverlay=pwm"
-#   }
-#
-#   dtoverlayCommand <- paste0(dtoverlayString, ",")
-#
-#   for(aPin in pin_number) {
-#       dtoverlayCommand <- paste0(dtoverlayCommand,
-#                                 "pin=", rpigpior::rpi_pinToBCM(aPin), ",",
-#                                 "func=", pwm_func(aPin), ","
-#                                 )
-#   }
-#   dtoverlayCommand <- substring(dtoverlayCommand, first = 0, last=nchar(dtoverlayCommand)-1)# remove trailing comma
-#
-#   print(dtoverlayCommand) # dtoverlayCommand contains the string to place in /boot/config.txt
-#
-# # end of mode=enable
-#
-#
-#
-#
-# pwm_func <- function(pin_number) {
-#   # helper function to convert pin to pwm function
-#   switch (as.character(pin_number),
-#           "12" = "2", # GPIO18, PWM0
-#           "32" = "4", # GPIO12, PWM0
-#           "33" = "4", # GPIO13, PWM1
-#           "35" = "2"  # GPIO19, PWM1
-# ) }
+
+pwm_func <- function(pin_number) {
+  # helper function to convert pin to pwm function
+  switch (as.character(pin_number),
+          "12" = "2", # GPIO18, PWM0
+          "32" = "4", # GPIO12, PWM0
+          "33" = "4", # GPIO13, PWM1
+          "35" = "2"  # GPIO19, PWM1
+) }
+
+
--- tests/testthat/test-rpi_pwm.R
+++ tests/testthat/test-rpi_pwm.R
@@ -1,3 +1,11 @@
-test_that("multiplication works", {
-  expect_equal(2 * 2, 4)
+test_that("is pin_number valid for use with PWM?", {
+  expect_error(rpi_pwm(30))
+})
+
+test_that("Do two PWM pins collide?", {
+  expect_error(rpi_pwm(c(12,32)))
+})
+
+test_that("length(pin_number) > 3", {
+  expect_error(rpi_pwm(c(12,32,33)))
 })
