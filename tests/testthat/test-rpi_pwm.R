0bea18cdf04c0eb54af6b16034ec91eb9390240c
Mark Niemann-Ross <mark.niemannross@gmail.com>
2023-09-19 19:44:24 -0700

    add pwm_debug=TRUE

--- tests/testthat/test-rpi_pwm.R
+++ tests/testthat/test-rpi_pwm.R
@@ -1,11 +1,11 @@
 test_that("is pin_number valid for use with PWM?", {
-  expect_error(rpi_pwm(30))
+  expect_error(rpi_pwm(30, pwm_debug = TRUE))
 })
 
 test_that("Do two PWM pins collide?", {
-  expect_error(rpi_pwm(c(12,32)))
+  expect_error(rpi_pwm(c(12,32), pwm_debug = TRUE))
 })
 
 test_that("length(pin_number) > 3", {
-  expect_error(rpi_pwm(c(12,32,33)))
+  expect_error(rpi_pwm(c(12,32,33), pwm_debug = TRUE))
 })
