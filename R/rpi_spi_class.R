13905b28caf66bd7caf04a62a292e646edb575b4
Mark Niemann-Ross <mark.niemannross@gmail.com>
2023-11-21 23:35:49 -0800

    added spiIncludes. fixed some {} syntax.

--- R/rpi_spi_class.R
+++ R/rpi_spi_class.R
@@ -1,19 +1,42 @@
-spi <- new_class("spi", properties = list(
-	spiChan = class_integer,
-	spiBus = class_integer,
-	max_speed_hz = class_integer
-),
+spiIncludes <- c("<sys/ioctl.h>",
+                 "<linux/types.h>",
+                 "<linux/spi/spidev.h>"
+                )
+
+spi <- new_class("spi",
+  properties = list(
+	  spiChan = class_integer,
+	  spiBus = class_integer,
+	  max_speed_hz = new_property(class_integer, default = "???defaultvalue???"),
+	  spiDeviceID = new_property(class_integer, default = "#rpi_spi_open")
+	  )
+,
 	validator = function(self) {
 		if (self@spiChan != 0 || self@spiChan != 1) {
-			"@spiChan must be zero or one"}
+			"@spiChan must be zero or one"
 		} else if (TRUE) {
-			"@spiBus must be something"}
+			"@spiBus must be something"
+		  }
+  }
+
 	)
 
-method(open, spi) <- function(x) {}
+# write.spi ---------------------------------------------------------------
+write.spi <- new_generic("rpi_spi_write", x)
 
-method(write, spi) <- function(x) {}
+method(rpi_spi_write, spi) <- function(x, stringToWrite) {}
 
-method(read, spi) <- function(x) {}
 
-method(close, spi) <- function(x) {}
+# read.spi ----------------------------------------------------------------
+read.spi <- new_generic("rpi_spi_read", x)
+
+method(rpi_spi_read, spi) <- function(x) {}
+
+
+# close  ------------------------------------------------------------------
+method(rpi_spi_close, spi) <- function(x) {
+  cppFunction('int rpi_spi_close(int x) {
+                  return close(x@spiDeviceID);
+              }',
+              includes = spiIncludes
+              )}
