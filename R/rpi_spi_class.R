28ab1d850ffbee7f921a5db3fdc675c0a7b7c99e
Mark Niemann-Ross <mark.niemannross@gmail.com>
2023-11-22 15:49:53 -0800

    trying to figure this out...

--- R/rpi_spi_class.R
+++ R/rpi_spi_class.R
@@ -1,4 +1,13 @@
-spiIncludes <- c("<sys/ioctl.h>",
+# https://www.rcpp.org
+# https://rdrr.io/cran/Rcpp/man/cppFunction.html
+# https://www.kernel.org/doc/html/latest/driver-api/ioctl.html
+
+# good example
+# https://raspberry-projects.com/pi/programming-in-c/spi/using-the-spi-interface
+
+spiIncludes <- c("<fcntl.h>",
+                 "<unistd.h>",
+                 "<sys/ioctl.h>",
                  "<linux/types.h>",
                  "<linux/spi/spidev.h>"
                 )
@@ -7,10 +16,15 @@ spi <- new_class("spi",
   properties = list(
 	  spiChan = class_integer,
 	  spiBus = class_integer,
-	  max_speed_hz = new_property(class_integer, default = "???defaultvalue???"),
-	  spiDeviceID = new_property(class_integer, default = "#rpi_spi_open")
-	  )
-,
+	  max_speed_hz = new_property(class_integer, default = "1000000"),
+	  spiDeviceID = new_property(class_integer, default = "#rpi_spi_open"),
+	  tx_buf        = something,
+	  rx_buf        = something,
+	  len           = count, # length of tx/rx buffers in bytes
+	  delay_usecs   = 0,
+	  bits_per_word = 8,
+	  cs_change     = 0
+	  	  ),
 	validator = function(self) {
 		if (self@spiChan != 0 || self@spiChan != 1) {
 			"@spiChan must be zero or one"
@@ -31,12 +45,13 @@ method(rpi_spi_write, spi) <- function(x, stringToWrite) {}
 read.spi <- new_generic("rpi_spi_read", x)
 
 method(rpi_spi_read, spi) <- function(x) {}
+#    err = ioctl(spiDeviceID, SPI_IOC_MESSAGE(1), &spi);
+
 
 
 # close  ------------------------------------------------------------------
-method(rpi_spi_close, spi) <- function(x) {
-  cppFunction('int rpi_spi_close(int x) {
+method(rpi_spi_close, spi) <- cppFunction('int rpi_spi_close(int x) {
                   return close(x@spiDeviceID);
               }',
               includes = spiIncludes
-              )}
+              )
