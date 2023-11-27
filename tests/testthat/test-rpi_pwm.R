ae6896ec0cf1fc2eaed9b734cf8c78832cee5b20
Mark Niemann-Ross <mark.niemannross@gmail.com>
2023-11-27 13:06:46 -0800

    Restore test for pwm

--- tests/testthat/test-rpi_pwm.R
+++ tests/testthat/test-rpi_pwm.R
@@ -1,11 +1,17 @@
+
 test_that("is pin_number valid for use with PWM?", {
+  skip_if_not(is.rpi())
   expect_error(rpi_pwm(30, pwm_debug = TRUE))
 })
 
 test_that("Do two PWM pins collide?", {
+  skip_if_not(is.rpi())
   expect_error(rpi_pwm(c(12,32), pwm_debug = TRUE))
 })
 
 test_that("length(pin_number) > 3", {
+  skip_if_not(is.rpi())
   expect_error(rpi_pwm(c(12,32,33), pwm_debug = TRUE))
 })
+
+test_that("real world test of pwm", {message("Testing PWM in real world")})
