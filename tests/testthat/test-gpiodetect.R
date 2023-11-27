31f4821e6ede74df7eac2768b57fb4fceaba1a49
Mark Niemann-Ross <mark.niemannross@gmail.com>
2023-11-27 13:18:54 -0800

    Unneeded tests since these R commands have been removed

--- tests/testthat/test-gpiodetect.R
+++
@@ -1,5 +0,0 @@
-test_that("gpiodetect returns the gpiochips", {
-  # this will obviously fail if libgpiod isn't installed
-  skip_if_not(is.rpi(), "gpiodetect - is.rpi() failed")
-  expect_no_error( gpiodetect())
-})
