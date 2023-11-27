98c099b42be578932331f123e2b678a2548f93ca
Mark Niemann-Ross <mark.niemannross@gmail.com>
2023-11-27 13:05:20 -0800

    restore test for is.rpi

--- tests/testthat/test-is.R
+++ tests/testthat/test-is.R
@@ -1,3 +1,11 @@
-test_that("multiplication works", {
-  expect_equal(2 * 2, 4)
+test_that("is.rpi confirms/denies RPi", {
+  expect_true(
+    # if /etc/os-release exists,
+    # ...and it contains "Raspbian"
+    # then is.rpi() should == TRUE
+    # else !is.rpi() should == TRUE
+    if(file.exists("/etc/os-release")){
+      if (any(grepl("Raspbian",readLines("/etc/os-release")))) {is.rpi()}
+    } else {!is.rpi()}
+  )
 })
