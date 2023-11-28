d770468545acb311c496771cd59ba1b2a99e0380
Mark Niemann-Ross <mark.niemannross@gmail.com>
2023-11-27 20:09:44 -0800

    split two tests

--- tests/testthat/test-rpi_pinToBCM.R
+++ tests/testthat/test-rpi_pinToBCM.R
@@ -1,4 +1,7 @@
 test_that("is pin correctly converted to BCM", {
   expect_named(rpi_pinToBCM(40), "GPIO21")
+})
+
+test_that("is pin correctly converted to BCM", {
   expect_error(rpi_pinToBCM(1))
 })
