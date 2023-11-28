4315c797e1a4e247d6c4e8df1e79b000cdeae1ad
Mark Niemann-Ross <mark.niemannross@gmail.com>
2023-11-27 20:10:03 -0800

    add real world test of pwm on channels 12 and 33

--- tests/testthat/test-rpi_pwm.R
+++ tests/testthat/test-rpi_pwm.R
@@ -14,4 +14,14 @@ test_that("length(pin_number) > 3", {
   expect_error(rpi_pwm(c(12,32,33), pwm_debug = TRUE))
 })
 
-test_that("real world test of pwm", {message("Testing PWM in real world")})
+test_that("real world test of pwm", {
+  skip_if_not(is.rpi())
+  message("Testing PWM in real world")
+  expect_silent({
+    rpi_pwm(pin_number=12, pwm_period = 50000, pwm_dutycycle = 10000)
+    Sys.sleep(1)
+    rpi_pwm(pin_number=33, pwm_period = 50000, pwm_dutycycle = 40000)
+    Sys.sleep(1)
+    rpi_pwm(pin_number=c(12,33), pwm_period = 0, pwm_dutycycle = 0)
+  })
+  })
