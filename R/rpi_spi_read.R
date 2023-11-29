4fb8b4fb0cc2d2206f369709b113a80b17e93b1e
Mark Niemann-Ross <mark.niemannross@gmail.com>
2023-11-29 03:40:32 -0800

    initial commit

--- R/rpi_spi_read.R
+++ R/rpi_spi_read.R
@@ -1,1 +1,14 @@
 # rpi_spi_read
+
+#' Reads an spi device
+#'
+#' @param spiControl list of spi device attributes as returned by rpi_spi_open()
+#'
+#' @return an spiControl list. spiControl$rx_buf contains the read value
+#' @export
+#'
+#' @examples
+rpi_spi_read <- function(spiControl) {
+  rpi_ioctl(spiControl$device, "SPI_IOC_MESSAGE(1)", spiControl)
+
+}
