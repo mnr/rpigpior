3c4a5e1eaa03db41d6862d2b2b13d86d8ec0a2a4
Mark Niemann-Ross <mark.niemannross@gmail.com>
2024-01-06 16:02:32 -0800

    removing set values

--- R/rpi_spi_open.R
+++ R/rpi_spi_open.R
@@ -1,5 +1,7 @@
 # rpi_spi_open
 
+# review this...
+# https://opencoursehub.cs.sfu.ca/bfraser/grav-cms/cmpt433/links/files/2022-student-howtos-ensc351/SPI-On-BBG.pdf
 # good example
 # https://raspberry-projects.com/pi/programming-in-c/spi/using-the-spi-interface
 
@@ -20,17 +22,25 @@
 #' @examples
 rpi_spi_open <- function(spiBus,
                          spiChan,
-                         spiMode,
-                         spiBits = 8,
-                         spiSpeed = 1000000) {
+                         spiMode = -1,
+                         spiBits = -1,
+                         spiSpeed = -1) {
   # I really ought to check these incoming values...
 
   spiDevString <- paste0("/dev/spidev",spiBus,".",spiChan)
   spiDeviceID <- file(description = spiDevString, open = "r+") # a connection object
 
+  # question. Should this be "open()" instead of "file()"
+  # fd = open("/dev/spidev0.0", O_RDWR);
+  # if (fd < 0)
+  # {
+  #   // error
+  # }
+
   spiControl <-  list(
     spiChan = spiChan,
     spiBus = spiBus,
+    spiMode = spiMode,
     max_speed_hz = spiSpeed,
     device = spiDeviceID, # a connection object
     tx_buf        = "", # text to send
@@ -38,32 +48,31 @@ rpi_spi_open <- function(spiBus,
     len           = 0, # length of tx/rx buffers in bytes
     delay_usecs   = 0,
     bits_per_word = spiBits,
-    cs_change     = 0
+    cs_change     = 0,
+    status_value = 0,
+    status_msg = ""
   )
 
-  # set the write ioctl mode
-  ioctl_value <- rpi_ioctl(spiDeviceID, "SPI_IOC_WR_MODE", spiControl)
-  if (ioctl_value$status_value == -1) {stop("Unable to set SPI mode: WR")}
+  if (spiMode == -1) {
+  # read ioctl/spi mode
+    returnSpiControl <- rpi_ioctl(spiDeviceID, "SPI_IOC_RD_MODE", spiControl)
+    if (returnSpiControl$status_value == -1) {stop(returnSpiControl$status_msg)}
+    spiControl$spiMode = returnSpiControl$spiMode;
+  }
 
-  # set the read ioctl mode
-  ioctl_value <- rpi_ioctl(spiDeviceID, "SPI_IOC_RD_MODE", spiControl)
-  if (ioctl_value$status_value == -1) {stop("Unable to set SPI mode: RD")}
+  if (spiBits == -1) {
+    # read the spi bitsPerWord
+    returnSpiControl <- rpi_ioctl(spiDeviceID, "SPI_IOC_RD_BITS_PER_WORD", spiControl)
+    if (returnSpiControl$status_value == -1) {stop(returnSpiControl$status_msg)}
+    spiControl$bits_per_word = returnSpiControl$bits_per_word;
+    }
 
-  # set the spi write bitsPerWord
-  ioctl_value <- rpi_ioctl(spiDeviceID, "SPI_IOC_WR_BITS_PER_WORD", spiControl)
-  if (ioctl_value$status_value == -1) {stop("Unable to set SPI mode: Write Bits Per Word")}
-
-  # set the spi read bitsPerWord
-  ioctl_value <- rpi_ioctl(spiDeviceID, "SPI_IOC_RD_BITS_PER_WORD", spiControl)
-  if (ioctl_value$status_value == -1) {stop("Unable to set SPI mode: Read Bits Per Word")}
-
-  # set the spi write speed
-  ioctl_value <- rpi_ioctl(spiDeviceID, "SPI_IOC_WR_MAX_SPEED_HZ", spiControl)
-  if (ioctl_value$status_value == -1) {stop("Unable to set SPI mode: Write speed")}
-
-  # set the spi read speed
-  ioctl_value <- rpi_ioctl(spiDeviceID, "SPI_IOC_RD_MAX_SPEED_HZ", spiControl)
-  if (ioctl_value$status_value == -1) {stop("Unable to set SPI mode: Read speed")}
+  if (spiSpeed == -1) {
+    # read the spi speed
+    returnSpiControl <- rpi_ioctl(spiDeviceID, "SPI_IOC_RD_MAX_SPEED_HZ", spiControl)
+  if (returnSpiControl$status_value == -1) {stop("returnSpiControl$status_msg")}
+    spiControl$max_speed_hz = returnSpiControl$max_speed_hz;
+  }
 
   return(spiControl)
 }
