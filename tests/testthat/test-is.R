6885db4df4cd31121113597247e742d7aef8be77
Mark Niemann-Ross <mark.niemannross@gmail.com>
2024-04-28 17:09:10 -0700

    there is no message on skip_on_os

--- tests/testthat/test-is.R
+++ tests/testthat/test-is.R
@@ -1,6 +1,6 @@
 test_that("is.rpi confirms/denies RPi", {
-  skip_on_os("mac", message = "This is a mac")
-  skip_on_os("windows", message = "this is windows")
+  skip_on_os("mac")
+  skip_on_os("windows")
   skip_if_not( file.exists("/etc/os-release"), message = "missing /etc/os-release")
   skip_if_not( any(grepl("Raspbian", readLines("/etc/os-release"))),
                message = "Raspbian is not in /etc/os-release")
