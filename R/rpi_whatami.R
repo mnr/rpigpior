07f2a0987812bcd56a5c1479329afc03d59eeb62
Mark Niemann-Ross <mark.niemannross@gmail.com>
2024-05-25 02:53:20 -0700

    add documentation, i2c_enabled and spi_enabled

---
+++ R/rpi_whatami.R
@@ -0,0 +1,57 @@
+# rpi_whatami returns a data.frame with information about the Raspberry Pi this program is running on
+
+#' rpi_whatami
+#'
+#' Identify the model of Raspberry Pi this code is running on
+#'    including useful environmental information
+#'
+#' @return
+#' A data.frame with information about this RPi
+#'
+#' * ...$is_rpi TRUE if this is a Raspberry Pi
+#' * ...$rpi_version "Zero", 1, 2, 3, 4, or 5
+#' * ...$gpiochip normally gpiochip0. For RPi 5, returns gpiochip4
+#' * ...$i2c_enabled TRUE if i2c is enabled on this RPi
+#' * ...$spi_enabled TRUE if spi is enabled on this RPi
+#'
+#' @export
+#'
+#' @examples
+rpi_whatami <- function() {
+  thisIsMe <- data.frame()
+
+  theResult <- tryCatch(
+    {
+      readLines("/proc/device-tree/model", warn = FALSE)
+    },
+    warning = function(w) {
+      FALSE
+    },
+    error = function(e) {
+      FALSE
+    },
+    finally = {
+      # cleanup
+    }
+  )
+
+  resultPieces <- unlist(strsplit(theResult," "))
+
+  thisIsMe$is_rpi <- ifelse(resultPieces[1] == "Raspberry",TRUE,FALSE)
+
+  thisIsMe$rpi_Version <- resultPieces[3] # model
+
+  # define which gpiochip to use
+  # all versions of Raspberry Pi use gpiochip0
+  # EXCEPT rpi 5, which uses gpiochip4
+  if (thisIsMe$rpi_Version == "5") {
+    thisIsMe$gpiochip <- "gpiochip4"
+  } else {
+    thisIsMe$gpiochip <- "gpiochip0"
+  }
+
+  thisIsMe$i2c_enabled <- file.exists("/dev/i2c-1")
+  thisIsMe$spi_enabled <- file.exists("/dev/spidev0.0")
+
+  return(thisIsMe)
+}
