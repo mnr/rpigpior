17c712c21a19b8a271bb064707a9bb465d215fe6
Mark Niemann-Ross <mark.niemannross@gmail.com>
2024-05-25 20:40:14 -0700

    correct the structure of trycatch

--- R/rpi_whatami.R
+++ R/rpi_whatami.R
@@ -8,6 +8,9 @@
 #' @return
 #' A data.frame with information about this RPi
 #'
+#' If this isn't a Raspberry Pi, ...$is_rpi will be FALSE. In addition, ...$fault will contain the error message
+#'
+#' If this is a Raspberry Pi, the data frame will contain the following:
 #' * ...$is_rpi TRUE if this is a Raspberry Pi
 #' * ...$rpi_version "Zero", 1, 2, 3, 4, or 5
 #' * ...$gpiochip normally gpiochip0. For RPi 5, returns gpiochip4
@@ -16,42 +19,45 @@
 #'
 #' @export
 #'
-#' @examples
+#' @examplesIf rpigpior::rpi_whatami()$is_rpi
+#' rpigpior::rpi_whatami()
+#'
+#'
 rpi_whatami <- function() {
   thisIsMe <- data.frame()
 
   theResult <- tryCatch(
     {
       readLines("/proc/device-tree/model", warn = FALSE)
+      resultPieces <- unlist(strsplit(theResult," "))
+      thisIsMe$is_rpi <- ifelse(resultPieces[1] == "Raspberry",TRUE,FALSE)
+      thisIsMe$rpi_Version <- resultPieces[3] # model
+
+      # define which gpiochip to use
+      # all versions of Raspberry Pi use gpiochip0
+      # EXCEPT rpi 5, which uses gpiochip4
+      if (thisIsMe$rpi_Version == "5") {
+        thisIsMe$gpiochip <- "gpiochip4"
+      } else {
+        thisIsMe$gpiochip <- "gpiochip0"
+      }
+
+      thisIsMe$i2c_enabled <- file.exists("/dev/i2c-1")
+      thisIsMe$spi_enabled <- file.exists("/dev/spidev0.0")
+      thisIsMe$fault <- ""
     },
     warning = function(w) {
-      FALSE
+      thisIsMe$is_rpi <- FALSE
+      thisIsMe$fault <- w
     },
     error = function(e) {
-      FALSE
+      thisIsMe$is_rpi <- FALSE
+      thisIsMe$fault <- e
     },
     finally = {
       # cleanup
     }
   )
-
-  resultPieces <- unlist(strsplit(theResult," "))
-
-  thisIsMe$is_rpi <- ifelse(resultPieces[1] == "Raspberry",TRUE,FALSE)
-
-  thisIsMe$rpi_Version <- resultPieces[3] # model
-
-  # define which gpiochip to use
-  # all versions of Raspberry Pi use gpiochip0
-  # EXCEPT rpi 5, which uses gpiochip4
-  if (thisIsMe$rpi_Version == "5") {
-    thisIsMe$gpiochip <- "gpiochip4"
-  } else {
-    thisIsMe$gpiochip <- "gpiochip0"
-  }
-
-  thisIsMe$i2c_enabled <- file.exists("/dev/i2c-1")
-  thisIsMe$spi_enabled <- file.exists("/dev/spidev0.0")
 
   return(thisIsMe)
 }
