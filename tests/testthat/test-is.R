722faf1c5268f85a801f89dd553adbef040ef956
Mark Niemann-Ross <mark.niemannross@gmail.com>
2024-05-25 20:40:59 -0700

    is.rpi() is deprecated

--- tests/testthat/test-is.R
+++
@@ -1,8 +0,0 @@
-test_that("is.rpi confirms/denies RPi", {
-  skip_on_os("mac")
-  skip_on_os("windows")
-  skip_if_not( file.exists("/etc/os-release"), message = "missing /etc/os-release")
-  skip_if_not( any(grepl("Raspbian", readLines("/etc/os-release"))),
-               message = "Raspbian is not in /etc/os-release")
-  expect_true( is.rpi() )
-})
