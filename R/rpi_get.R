03b27a3edb4efdc3235a0567655ab1e0e5a41a56
Mark Niemann-Ross <mark.niemannross@gmail.com>
2024-05-27 10:50:28 -0700

    add documentation and examples for the use of rpi_whatami()

--- R/rpi_get.R
+++ R/rpi_get.R
@@ -3,6 +3,10 @@
 #' Returns the value of one or more specified board-level pin number (0-40). Stops if the pin is not a valid data line.
 #'
 #' @param pin_number a Raspberry Pi GPIO board level pin number between 1 and 40. For example, Pin #1 is located upper left and provides 3.3 volts. Pin #40 is located lower right and is the location of GPIO21.
+#' @param whatami a data.frame of configuration data as supplied by rpi_whatami().
+#'    The default value is a call to rpi_whatami(). If you make frequent calls to
+#'    to this function, consider saving the result of rpi_whatami() and pass that saved
+#'    data.frame instead.
 #'
 #' @return named vector with the value of each pin number.
 #' @export
@@ -13,6 +17,10 @@
 #' }
 #' rpi_get(40) # returns a named vector of the state of GPIO21. i.e. return["GPIO21"] = 1
 #' rpi_get(c(7, 40)) # returns a named vector of values from pins 7 and 40
+#'
+#' # consider this for multiple calls to rpi_get()
+#' mywhatami <- rpi_whatami()
+#' rpi_get(40, whatami = mywhatami)
 rpi_get <- function(pin_number, whatami = rpi_whatami()) {
 
   bcm_line <- rpigpior::rpi_pinToBCM(pin_number)
--- R/rpi_set.R
+++ R/rpi_set.R
@@ -4,6 +4,10 @@
 #'
 #' @param pin_number one or more pin numbers as found on the Raspberry pi GPIO. Use the actual pin numbers (i.e. 1:40)
 #' @param onOff a vector the same length of pin_number with values of 0 or 1 to turn the line off or on
+#' @param whatami a data.frame of configuration data as supplied by rpi_whatami().
+#'    The default value is a call to rpi_whatami(). If you make frequent calls to
+#'    to this function, consider saving the result of rpi_whatami() and pass that saved
+#'    data.frame instead.
 #'
 #' @return no value is returned
 #' @export
@@ -19,6 +23,11 @@
 #' rpi_set(toggleThesePins, c(0, 1, 0)) # turns off pins 19 and 23
 #' Sys.sleep(1)
 #' rpi_set(toggleThesePins, 0) # turns off all pins
+#'
+#' # consider this for multiple calls to rpi_set()
+#' mywhatami <- rpi_whatami()
+#' rpi_set(19, whatami = mywhatami)
+
 rpi_set <- function(pin_number, onOff, whatami = rpi_whatami()) {
   if (is.logical(onOff)) {
     onOff <- ifelse(onOff, 1, 0)
